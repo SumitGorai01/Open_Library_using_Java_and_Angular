60463cdf638456af21873fc7242fecf318276e87
Sumit <goraisumit01@gmail.com>
2024-07-21 13:51:59 +0530

    solve error in project

--- OpenLibrary-front/src/app/component/navbar/navbar.component.ts
+++ OpenLibrary-front/src/app/component/navbar/navbar.component.ts
@@ -1,9 +1,6 @@
 import { Component, HostListener, OnInit } from '@angular/core';
 import { LoginService } from '../../services/login.service';
-<<<<<<< HEAD
 import Swal from 'sweetalert2';
-=======
->>>>>>> origin/main
 
 @Component({
   selector: 'app-navbar',
@@ -32,7 +29,6 @@ export class NavbarComponent implements OnInit {
     this.login.logout();
     // this.isLoggedIn=false;
     // this.user=null;
-<<<<<<< HEAD
 
     window.location.href='/';
     Swal.fire({
@@ -41,8 +37,6 @@ export class NavbarComponent implements OnInit {
       title: "Logout Successfully !!",
       timer: 3000
     });
-=======
     window.location.reload();
->>>>>>> origin/main
   }
 }
